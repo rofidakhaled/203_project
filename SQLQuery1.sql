8a6a2fa7431d5f0c10eccbc9f893d77fc718844e
rofidakhaled <149782421+rofidakhaled@users.noreply.github.com>
2023-12-18 03:43:41 +0200

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,89 @@
+
+create table pomodoro(
+sess_id int NOT NULL,
+sess_name varchar (255),
+sess_time int NOT NULL,
+short_break int NOT NULL,
+long_break int NOT NULL,
+frequency int NOT NULL,
+timeslot datetime NOT NULL,
+
+primary key (sess_id)
+
+);
+
+create table todo(
+todo_id int NOT NULL,
+todo_name nvarchar,
+sub_todo int,
+checked int,
+timeslot datetime NOT NULL,
+primary key (todo_id)
+
+);
+
+create table reminder(
+rem_id int NOT NULL,
+rem_name nvarchar,
+set_date datetime NOT NULL,
+timeslot datetime NOT NULL,
+primary key (rem_id)
+
+);
+
+create table notes
+(
+st_id int NOT NULL,
+st_name nvarchar,
+timeslot datetime NOT NULL,
+primary key (st_id)
+
+);
+
+create table callender (
+event_id int NOT NULL,
+event_name varchar(255),
+event_type varchar (255) NOT NULL,
+timeslot datetime NOT NULL,
+primary key (event_id)
+
+);
+
+create table habits(
+habit_id int NOT NULL,
+habit_name varchar(255),
+Frequency int,
+perday int,
+c_num int,
+timeslot datetime NOT NULL,
+primary key (habit_id)
+
+);
+
+create table emotion(
+emo_id int NOT NULL,
+emo varchar(255),
+timeslot datetime NOT NULL,
+primary key (emo_id)
+);
+
+
+select todo_name 
+from todo
+where checked >0
+
+select sub_todo
+from todo 
+where todo_id =1
+
+select rem_name
+from reminder 
+where set_date = 12/18/2023
+
+select count(*)
+from callender
+where event_type = 'bday'
+
+select habit_name
+from habits
+where c_num > Frequency or c_num = frequency
