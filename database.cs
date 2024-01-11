fb507569275ccee49fe38d97543704df0ea922a7
Rehab Mahmoud <124354951+Rehab-mahmoud@users.noreply.github.com>
2024-01-11 13:12:41 +0200

    Add files via upload

--- database.cs
+++ database.cs
@@ -14,16 +14,18 @@ namespace WebApplication9.Models
             con = new SqlConnection(conSTR);
         }
 
-        public void AddHabit(int User,int habitID, int frequency,string habitName,DateTime stratdate,DateTime endDate)
+        public void AddHabit(int User,int habitID,string habitName,DateTime date)
         {
             DataTable dt = new DataTable();
-            string Query = $"Insert into Habits(habit_id,userID,Frequency,habit_name,startDate,EndDate) VALUES (@HABITid,@USERid,@frequency,@HABITname,@StartDate,@endDate)";
+            string Query = $"Insert into Habits(habit_id,userID,habit_name,HabitStatus,completeDate) VALUES (@HABITid,@USERid,@HABITname,'Uncomlete',@Date)";
             try
             {
                 con.Open();
                 SqlCommand cmd = new SqlCommand(Query, con);
                 cmd.Parameters.AddWithValue("@HABITid", habitID);
                 cmd.Parameters.AddWithValue("@USERid", User);
+                cmd.Parameters.AddWithValue("HABITname", habitName);
+                cmd.Parameters.AddWithValue("Date", date);
                 cmd.ExecuteNonQuery();
             }
             catch (Exception e)
@@ -65,7 +67,7 @@ namespace WebApplication9.Models
         {
             string answer=null;
             string query =
-                "INSERT INTO users(Email,user_Password,User_Name,User_ID) VALUES(@email,@PASSWORD,@USERNAME,(select count(*) from users)+1)";
+                "INSERT INTO users(User_Email,User_Password,User_Name,User_ID) VALUES(@email,@PASSWORD,@USERNAME,(select count(*) from users)+1)";
             try
             {
 
@@ -87,6 +89,61 @@ namespace WebApplication9.Models
             }
 
             return answer;
+        }
+
+        public string? AddEvent(string occassion, int year, int month,int day)
+        {
+            string insertion=null;
+            string query =
+                "INSERT INTO calender(event_id,event_name,timeslot) VALUES((SELECT count(*) from calender)+1,@Name,DATEFROMPARTS(@year,@month,@day))";
+            try
+            {
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Name", occassion);
+                cmd.Parameters.AddWithValue("@year",year );
+                cmd.Parameters.AddWithValue("@month",month );
+                cmd.Parameters.AddWithValue("@day",day );
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                insertion = e.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return insertion;
+        }
+
+        public int returnID(string Email)
+        {
+            int ID;
+            DataTable dt = new DataTable();
+            string Query = $"SELECT USER_ID from users where User_Email={Email}";
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(Query, con);
+                ID = (int)cmd.ExecuteScalar();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return ID;
         }
 
     }
